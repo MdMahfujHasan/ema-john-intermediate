ebeebda9571af6124463a47641f9b30b6da3f169
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-05-11 19:53:15 +0600

    applied jobs no added

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,21 +1,28 @@
 import { getShoppingCart } from "../../utilities/fakedb";
 
-/* eslint-disable */
 const cartProductsLoader = async () => {
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
 
     // if cart data is in database, you have to use async await
     const storedCart = getShoppingCart();
+
     const savedCart = [];
+
     for (const id in storedCart) {
-        const addedProducts = products.find(pd => pd.id === id);
-        if (addedProducts) {
+        const addedProduct = products.find(pd => pd.id === id);
+        if (addedProduct) {
             const quantity = storedCart[id];
-            addedProducts.quantity = quantity;
-            savedCart.push(addedProducts);
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
         }
     }
+
+    // if you need to send two things
+    // return [products, savedCart]
+    // another options
+    // return { products, cart: savedCart }
+
     return savedCart;
 }
 
